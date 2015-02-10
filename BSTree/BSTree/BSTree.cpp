d829c3c79c37ee59ff255008a972f31f673cea29
Mike Koch <mkoch227@gmail.com>
2015-02-10 10:44:30 -0500

    Add parent pointer, function prototypes, and blank methods

--- BSTree/BSTree/BSTree.cpp
+++ BSTree/BSTree/BSTree.cpp
@@ -9,20 +9,35 @@ struct Node
 	string value;
 	Node* leftChild;
 	Node* rightChild;
+	Node* parent;
 };
 struct Tree
 {
 	Node* root;
 };
-int main() {
 
-	// Keep looping until the user enters "exit"
+//-- Function prototypes
+string search(Tree* tree, string key);
+string insertValue(Tree* tree, string key);
+string deleteValue(Tree* tree, string key);
+string min(Tree* tree);
+string max(Tree* tree);
+string next(Tree* tree, string key);
+string previous(Tree* tree, string key);
+string list(Tree* tree);
+string help();
+
+int main() {
+	//-- Create a new, empty tree to start with
+	Tree* tree = new Tree();
+
+	//-- Keep looping until the user enters "exit"
 	while (true) {
-		// Take user input from the console
+		//-- Take user input from the console
 		string input;
 		cin >> input;
 
-		// The transform method will convert the input string to all lowercase, making it easier to evaluate.
+		//-- The transform method will convert the input string to all lowercase, making it easier to evaluate.
 		transform(input.begin(), input.end(), input.begin(), tolower);
 
 		//-- Go to the appropriate action the user requested
@@ -55,8 +70,7 @@ int main() {
 			cout << "Entered list" << endl;
 			//-- go to list
 		} else if (input == "help") {
-			cout << "Entered help" << endl;
-			//-- go to help
+			cout << help();
 		} else if (input == "exit") {
 			//-- Exit the application
 			return 0;
@@ -65,4 +79,49 @@ int main() {
 			cout << "The input '" + input + "' is invalid. Enter 'help' for a list of commands." << endl;
 		}
 	}
+}
+
+// Searches for the value in the tree
+string search(Tree* tree, string key) {
+	return "";
+}
+
+// Inserts the string into the tree
+string insertValue(Tree* tree, string key) {
+	return "";
+}
+
+// Deletes the string from the tree
+string deleteValue(Tree* tree, string key) {
+	return "";
+}
+
+// Outputs the text with the smallest value (ex. a < z)
+string min(Tree* tree) {
+	return "";
+}
+
+// Outputs the text with the largest value (ex z > a)
+string max(Tree* tree) {
+	return "";
+}
+
+// Outputs the item that is directly next to the string entered
+string next(Tree* tree, string key) {
+	return "";
+}
+
+// Outputs the item that is directly previous to the string entered
+string previous(Tree* tree, string key) {
+	return "";
+}
+
+// Outputs an in-order traversal of the tree in its current form
+string list(Tree* tree) {
+	return "";
+}
+
+// Outputs help text to the user, showing a list of available commands
+string help() {
+	return "TODO: Help text.";
 }
