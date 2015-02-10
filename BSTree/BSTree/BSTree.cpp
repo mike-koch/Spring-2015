7f15b9845bb4ac217d6e9701ed20879831b6f549
Mike Koch <mkoch227@gmail.com>
2015-02-09 21:57:27 -0500

    Commands from the terminal are now recognized

--- BSTree/BSTree/BSTree.cpp
+++ BSTree/BSTree/BSTree.cpp
@@ -1,5 +1,6 @@
-#include <iostream>;
-#include <string>;
+#include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -10,28 +11,47 @@ int main() {
 		string input;
 		cin >> input;
 
-		// Depending on input, go to proper action.
-		// if (action == 'SEARCH'), etc...
-		//-- each method is responsible for their actions and will return appropriately.
+		// The transform method will convert the input string to all lowercase, making it easier to evaluate.
+		transform(input.begin(), input.end(), input.begin(), tolower);
+
+		//-- Go to the appropriate action the user requested
+		if (input == "search") {
+			cout << "Entered search" << endl;
+			//-- go to search
+		} else if (input == "insert") {
+			cout << "Entered insert" << endl;
+			//-- go to insert
+		} else if (input == "delete") {
+			cout << "Entered delete" << endl;
+			//-- go to delete
+		} else if (input == "min") {
+			cout << "Entered min" << endl;
+			//-- go to min
+		} else if (input == "max") {
+			cout << "Entered max" << endl;
+			//-- go to max
+		} else if (input == "next") {
+			string stringToFind;
+			cin >> stringToFind;
+			cout << "Entered next with stringToFind = " << stringToFind << endl;
+			//-- go to next
+		} else if (input == "prev") {
+			string stringToFind;
+			cin >> stringToFind;
+			cout << "Entered prev with stringToFind = " << stringToFind << endl;
+			//-- go to previous
+		} else if (input == "list") {
+			cout << "Entered list" << endl;
+			//-- go to list
+		} else if (input == "help") {
+			cout << "Entered help" << endl;
+			//-- go to help
+		} else if (input == "exit") {
+			//-- Exit the application
+			return 0;
+		} else {
+			//-- User entered invalid input. Notify the user that their input was invalid and have them prompt again.
+			cout << "The input '" + input + "' is invalid. Enter 'help' for a list of commands." << endl;
+		}
 	}
-
-	return 0;
 }
-
-Action getAction(string input) {
-	// Sample Action to return.
-	return Action::INSERT;
-}
-
-enum Action {
-	INSERT,
-	DELETE,
-	SEARCH,
-	MIN,
-	MAX,
-	NEXT,
-	PREV,
-	LIST,
-	HELP,
-	EXIT
-};
