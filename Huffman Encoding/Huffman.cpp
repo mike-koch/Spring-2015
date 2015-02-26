9f12330c31668d6fa5e12e24612f70c27e387ce7
Mike Koch <mkoch227@gmail.com>
2015-02-26 01:14:11 -0500

    Committing in a bad place... stack overflow error on the traversal :(

--- Huffman Encoding/Huffman.cpp
+++ Huffman Encoding/Huffman.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 #include "Huffman.h"
 
+Node* nodeArray[256];
+Node* root;
 //-- PUBLIC methods
 
 // Builds a Huffman tree based on each byte processed. Each byte will be one of the ASCII characters with an int value of 0-255.
@@ -22,14 +24,13 @@ void Huffman::initializeFromFile(string fileName) {
 	}
 
 	//-- Initialize an array to contain all 255 ASCII characters nodes.
-	Node* nodeArray[256];
-	initializeArray(nodeArray);
+	initializeNodeArray();
 
 	//-- Process the input file with the array
-	processFile(inputFile, nodeArray);
+	processFile(inputFile);
 
 	//-- Build the huffman tree
-	buildTree(nodeArray);
+	buildTree();
 }
 
 /*
@@ -38,7 +39,10 @@ void Huffman::initializeFromFile(string fileName) {
    Requires: initializeFromFile must have been ran before encoding; otherwise there is no tree to use as the key!
 */
 void Huffman::encodeFile(string inFile, string outFile) {
-	//placeholder
+	//-- First, build an array of ASCII character to encoding string that we can use to encode our file.
+	string encodingArray[256];
+	buildEncodingArray(encodingArray, root, "");
+	string throwaway = "debugging...";
 }
 
 /*
@@ -49,15 +53,16 @@ void Huffman::decodeFile(string inFile, string outFile) {
 }
 
 //-- PRIVATE functions
-void Huffman::initializeArray(Node* nodeArray[256]) {
+void Huffman::initializeNodeArray() {
 	for (int i = 0; i < 256; i++) {
 		Node* node = new Node();
+		node->key = (unsigned char)i;
 		node->weight = 0;
 		nodeArray[i] = node;
 	}
 }
 
-void Huffman::processFile(ifstream& inputFile, Node* nodeArray[256]) {
+void Huffman::processFile(ifstream& inputFile) {
 	//-- Process each symbol, by incrementing the node's count.
 	while (!inputFile.eof()) {
 		char symbol;
@@ -67,11 +72,11 @@ void Huffman::processFile(ifstream& inputFile, Node* nodeArray[256]) {
 }
 
 //-- Builds the tree by taking the smallest nodes and giving them a parent. Then, the process keeps looping until a full tree has been formed.
-void Huffman::buildTree(Node* nodeArray[256]) {
-	while (!treeIsBuilt(nodeArray)) {
+void Huffman::buildTree() {
+	while (!treeIsBuilt()) {
 		//-- Get the smallest two nodes in the array
-		int firstMinIndex = min(nodeArray);
-		int secondMinIndex = min(nodeArray, firstMinIndex);
+		int firstMinIndex = min();
+		int secondMinIndex = min(firstMinIndex);
 		Node* firstMin = nodeArray[firstMinIndex];
 		Node* secondMin = nodeArray[secondMinIndex];
 
@@ -86,16 +91,18 @@ void Huffman::buildTree(Node* nodeArray[256]) {
 		//-- Set firstMinIndex to the new parent node, and set secondMinIndex to NULL.
 		nodeArray[firstMinIndex] = parentNode;
 		nodeArray[secondMinIndex] = NULL;
+
+		root = parentNode;
 	}
 
-	//-- The tree has been built
+	//-- The tree has been built. The root was set to firstMin at the very end of the loop, so we also have our root node.
 	return;
 }
 
 //-- Searches for the node with the smallest weight, excluding the node specified as the exclusion. 
 //   A NULL exclusion means to not exclude a node.
 //   Returns: the array index of the min.
-int Huffman::min(Node* nodeArray[256], int exclusionIndex) {
+int Huffman::min(int exclusionIndex) {
 	//-- Set the minWeight to the max, so the first MUST be smaller than INT_MAX.
 	int minWeight = INT_MAX;
 	int minIndex = -1;
@@ -110,7 +117,7 @@ int Huffman::min(Node* nodeArray[256], int exclusionIndex) {
 }
 
 //-- Returns true if the Huffman tree has been built (all indices are NULL except for one)
-bool Huffman::treeIsBuilt(Node* nodeArray[256]) {
+bool Huffman::treeIsBuilt() {
 	bool encounteredNotNullIndex = false;
 	for (int i = 0; i < 256; i++) {
 		if (nodeArray[i] != NULL) {
@@ -125,3 +132,37 @@ bool Huffman::treeIsBuilt(Node* nodeArray[256]) {
 	//-- If we got here, only one index was not null. Return true;
 	return true;
 }
+
+int stackCount = 0;
+void Huffman::buildEncodingArray(string encodingArray[256], Node* startingNode, string encodedValue) {
+	stackCount++;
+	//-- Perform an in-order traversal of the tree, keeping track of each branch taken (0 = left, 1 = right). Once we find a leaf node,
+	//   mark the encoding string for it's ASCII character.
+
+	// check if we're at a leaf node (no left/right children). If so, set the array's encoded value
+	/*if (startingNode->leftChild == NULL && startingNode->rightChild == NULL) {
+		encodingArray[(unsigned char)startingNode->key] = encodedValue;
+	}
+	
+	// start by going left, only if not null.
+	if (startingNode->leftChild != NULL) {
+		//-- Now go left, adding a 0 to the encodedValue
+		buildEncodingArray(encodingArray, startingNode->leftChild, encodedValue += "0");
+	}
+	// we know that this isn't a leaf node, since that was checked earlier. So there's nothing to do with this node. Just go to the right now.
+	if (startingNode->rightChild != NULL) {
+		//-- Now go right, adding a 1 to the encodedValue
+		buildEncodingArray(encodingArray, startingNode->rightChild, encodedValue += "1");
+	}*/
+	string throwawayArray[256];
+
+	if (startingNode == NULL) {
+		stackCount--;
+		return;
+	}
+
+	buildEncodingArray(throwawayArray, startingNode->leftChild, "");
+	cout << to_string(startingNode->key) + to_string(startingNode->weight) << endl;
+	buildEncodingArray(throwawayArray, startingNode->rightChild, "");
+}
+
--- Huffman Encoding/Huffman.h
+++ Huffman Encoding/Huffman.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 struct Node {
-	char key;
+	unsigned char key;
 	int weight;
 	Node* leftChild;
 	Node* rightChild;
@@ -14,9 +14,10 @@ public:
 	void decodeFile(string inFile, string outFile);
 
 private:
-	void initializeArray(Node* nodeArray[256]);
-	void processFile(ifstream& inputFile, Node* nodeArray[256]);
-	void buildTree(Node* nodeArray[256]);
-	int min(Node* nodeArray[256], int exclusionIndex = INT_MAX);
-	bool treeIsBuilt(Node* nodeArray[256]);
+	void initializeNodeArray();
+	void processFile(ifstream& inputFile);
+	void buildTree();
+	int min(int exclusionIndex = INT_MAX);
+	bool treeIsBuilt();
+	void buildEncodingArray(string encodingArray[256], Node* startingNode, string encodedValue);
 };
