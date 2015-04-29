4d0ff3828c46ab7140a9e187fc9685793bc4b2b0
Mike Koch <mkoch227@gmail.com>
2015-04-28 21:57:22 -0400

    Initial commit

---
+++ Minimum-Spanning-Tree/Main.cpp
@@ -0,0 +1,24 @@
+// Mike Koch - Minimum Spanning Tree
+// EECS 2510 | 04/28/2015
+// Main.cpp - Responsible for parsing input and executing required tree algorithms
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numberOfHeaders;
+	cin >> numberOfHeaders;
+
+	// There will be N nodes for N numberOfHeaders
+	for (int i = 0; i < numberOfHeaders; i++)
+	{
+		// create vertex
+	}
+
+	// for the next N x N characters create an adjacency matrix
+	for (int i = 0; i < numberOfHeaders * numberOfHeaders; i++)
+	{
+
+	}
+}
